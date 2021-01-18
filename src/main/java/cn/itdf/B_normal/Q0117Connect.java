867755a3cc1cf9661682a40bdbf7cd479b924902
zhuwenkang <zhuwenkang@pconline.com.cn>
2021-01-18 20:49:45 +0800

    update

--- src/main/java/cn/itdf/B_normal/Q0117Connect.java
+++ src/main/java/cn/itdf/B_normal/Q0117Connect.java
@@ -3,36 +3,34 @@ package cn.itdf.B_normal;
 import cn.itdf.base.Node;
 
 public class Q0117Connect {
+    Node last, nextStart;
+
     public Node connect(Node root) {
         if (root == null) return root;
-        Node leftMost = root;
-        while (leftMost.left != null) {
-            Node head = leftMost;
-            while (head != null) {
-                Node node = null;
-                if (head.right != null) {
-                    if (head.left != null) {
-                        head.left.next = head.right;
-                    }
-                    node = head.right;
-                }else if (head.left != null) {
-                    node = head.left;
+        Node head = root;
+        while (head != null) {
+            last = null;
+            nextStart = null;
+            for (Node node = head; node != null; node = node.next) {
+                if (node.left != null) {
+                    bfs(node.left);
                 }
-                Node tempNode = head;
-                while (node != null && node.next == null && tempNode.next != null) {
-                    tempNode = tempNode.next;
-                    if (tempNode.left != null) {
-                        node.next = tempNode.left;
-                    }else if (tempNode.right != null) {
-                        node.next = tempNode.right;
-                    }
+                if (node.right != null) {
+                    bfs(node.right);
                 }
-                head = head.next;
             }
-            leftMost = leftMost.left;
+            head = nextStart;
         }
-        connect(root.left);
-        connect(root.right);
         return root;
+    }
+
+    public void bfs(Node node) {
+        if (last != null) {
+            last.next = node;
+        }
+        if (nextStart == null) {
+            nextStart = node;
+        }
+        last = node;
     }
 }
